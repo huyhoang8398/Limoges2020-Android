6a59b59f70ccc213fe469c39cbaef8cffbac0a09
kn <huyhoang8398@gmail.com>
2020-09-18 00:38:11 +0200

    TP1

--- TP1/excercise1/app/src/main/java/com/example/excercise1/MainActivity.java
+++ TP1/excercise1/app/src/main/java/com/example/excercise1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.excercise1;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,81 +10,44 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private final int min = 0;
-    final int max = 100;
-    final int random = new Random().nextInt((max - min) + 1) + min;
-    RadioGroup radioGroup;
-    RadioButton radioButton;
-    EditText eurMoney;
-
+    private int min;
+    private int max;
+    private int random;
     private int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println(random);
-
+        // init variable
+        this.min = 0;
+        this.max = 100;
+        this.random = new Random().nextInt((max - min) + 1) + min;
+        this.count = 0;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        final EditText dollarMoney = (EditText) findViewById(R.id.dollar);
-        eurMoney = (EditText) findViewById(R.id.eur);
-
-        radioGroup = (RadioGroup) findViewById(R.id.radio);
-
-        this.count = 0;
-
-        Button btnConvert = (Button) findViewById(R.id.convertMoney);
-        btnConvert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-
-                // find the radiobutton by returned id
-                radioButton = (RadioButton) findViewById(selectedId);
-
-                if (radioButton.getText().equals("Dol->Eu")) {
-                    float dollar = Float.parseFloat(dollarMoney.getText().toString());
-                    System.out.println(dollar);
-                    float eurConverted = convertToEuro(dollar);
-                    System.out.println(eurConverted);
-                    eurMoney.setText(String.valueOf(eurConverted));
-                }
-
-                if (radioButton.getText().equals("Eu->Dol")) {
-                    float eurmoney = Float.parseFloat(eurMoney.getText().toString());
-                    float dollarConverted = convertToDollar(eurmoney);
-                    dollarMoney.setText(String.valueOf(dollarConverted));
-                }
-
-                Toast.makeText(MainActivity.this,
-                        radioButton.getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
-
-    public void play(View view){
-        //final TextView guessTextView = (TextView) findViewById(R.id.guessNum);
-        //guessTextView.setText(String.valueOf(random));
-        //guessTextView.setVisibility(View.GONE);
-
+    public void play (View view){
         final EditText guessText = (EditText) findViewById(R.id.guessText);
-        int guess = Integer.parseInt(guessText.getText().toString());
-//        System.out.println(guess);
-        actionGuess(guess);
-        count++;
-        String toast = "count: " + String.valueOf(count);
-        Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
-
+        String guess_text = guessText.getText().toString().trim();
+        if(guess_text.isEmpty())
+        {
+            Toast.makeText(this, "You need to enter the guessing number", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            int guess = Integer.parseInt(guessText.getText().toString());
+            actionGuess(guess);
+            count++;
+            String toast = "count: " + String.valueOf(count);
+            Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void actionGuess(int guess) {
+    public void actionGuess ( int guess){
         if (guess > random) {
             Toast.makeText(getApplicationContext(), "Your number is bigger", Toast.LENGTH_SHORT).show();
         } else if (guess < random) {
@@ -91,21 +55,43 @@ public class MainActivity extends AppCompatActivity {
         } else {
             final TextView guessTextView = (TextView) findViewById(R.id.guessNum);
             guessTextView.setVisibility(View.VISIBLE);
-
             Toast.makeText(getApplicationContext(), "Win", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public static float convertToEuro(float amount)
-    {
-        float euro = (float) (amount / 1.18);
-        return euro;
+    public void convert(View view) {
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        final RadioButton radioButton = (RadioButton) findViewById(selectedId);
+        final EditText dollarMoney = (EditText) findViewById(R.id.dollar);
+        final EditText eurMoney = (EditText) findViewById(R.id.eur);
+        // find the radiobutton by returned id
+
+        if (radioButton.getText().equals("Dol->Eu")) {
+            if (TextUtils.isEmpty(dollarMoney.getText().toString())) {
+                Toast.makeText(this, "You need to enter the amount of dollar", Toast.LENGTH_SHORT).show();
+            } else {
+                float dollar = Float.parseFloat(dollarMoney.getText().toString());
+                float eurConverted = convertToEuro(dollar);
+                eurMoney.setText(String.valueOf(eurConverted));
+            }
+        }
+        else if (radioButton.getText().equals("Eu->Dol")) {
+            if (TextUtils.isEmpty(eurMoney.getText().toString())) {
+                Toast.makeText(this, "You need to enter the amount of eur", Toast.LENGTH_SHORT).show();
+            } else {
+                float euro = Float.parseFloat(eurMoney.getText().toString());
+                float dollarConverted = convertToDollar(euro);
+                dollarMoney.setText(String.valueOf(dollarConverted));
+            }
+        }
     }
-    public static float convertToDollar(float amount) {
-        float dollar  = (float) (amount * 1.36077);
-        return dollar;
+
+    public static float convertToEuro ( float amount)
+    {
+        return (float) (amount / 1.18);
+    }
+    public static float convertToDollar ( float amount){
+        return (float) (amount * 1.36077);
     }
 }
-
-
-
