1bbf39051b6931b67cc058f74c5796c4bc5d827e
kn <huyhoang8398@gmail.com>
2020-09-22 19:22:20 +0200

    TP3: ex1 done

--- TP3/Excersise3/app/src/main/java/com/example/excersise2/DecryptActivity.java
+++ TP3/Excersise3/app/src/main/java/com/example/excersise2/DecryptActivity.java
@@ -1,8 +1,11 @@
 package com.example.excersise2;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,29 +17,37 @@ public class DecryptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_decrypt);
     }
 
-
     public void decrypt(View view) {
         final EditText decryptText = (EditText) findViewById(R.id.decryptText);
+        final EditText decryptKey = (EditText) findViewById(R.id.decryptKey);
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.decryptRadioGroup);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        final RadioButton radioButton = (RadioButton) findViewById(selectedId);
+
+
         String text = decryptText.getText().toString();
-        StringBuilder result = new StringBuilder();
+        String key = decryptKey.getText().toString();
         if (text.isEmpty()) {
-            Toast.makeText(this, "You need to enter text to encrypt", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            for (char character : text.toCharArray()) {
-                if (character != ' ') {
-                    int originalAlphabetPosition = character - 'a';
-                    int newAlphabetPosition = (originalAlphabetPosition + 23) % 26;
-                    System.out.println(newAlphabetPosition);
-                    char newCharacter = (char) ('a' + newAlphabetPosition);
-                    result.append(newCharacter);
+            Toast.makeText(this, "You need to enter text to decrypt", Toast.LENGTH_SHORT).show();
+        } else {
+            if (radioButton.getText().equals("Caesar Cipher Decrypt")) {
+                if (TextUtils.isEmpty(decryptKey.getText().toString())) {
+                    Toast.makeText(this, "You need to enter the key", Toast.LENGTH_SHORT).show();
                 } else {
-                    result.append(character);
+                    Intent intent = new Intent(DecryptActivity.this, ResultDecryptActivity.class);
+                    intent.putExtra("param", new Parameter(text, key, "decrypt", "caesar"));
+                    startActivity(intent);
                 }
             }
-            Intent intent = new Intent(DecryptActivity.this, ResultDecryptActivity.class);
-            intent.putExtra("result", result.toString());
-            startActivity(intent);
+            if (radioButton.getText().equals("Vigenere Cipher Decrypt")) {
+                if (TextUtils.isEmpty(decryptKey.getText().toString())) {
+                    Toast.makeText(this, "You need to enter the key", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(DecryptActivity.this, ResultDecryptActivity.class);
+                    intent.putExtra("param", new Parameter(text, key, "decrypt", "vigenere"));
+                    startActivity(intent);
+                }
+            }
         }
     }
 }
